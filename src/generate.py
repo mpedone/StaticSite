5c37c52881cda5caa5091cb66adee809b62c202d
Matthew Pedone <12798189+mpedone@users.noreply.github.com>
2025-09-11 15:02:25 -0400

    set repo up for GitHub to host site

--- src/generate.py
+++ src/generate.py
@@ -12,18 +12,18 @@ def extract_title(markdown):
     title = header.group(1)
     return title
 
-def generate_page_recursively(from_path, template_path, dest_path):
+def generate_page_recursively(from_path, template_path, dest_path, basepath):
     files_list = os.listdir(from_path)
     for file in files_list:
         new_from_path = os.path.join(from_path, file)
         new_dest_path = os.path.join(dest_path, file)
         if os.path.isfile(new_from_path):
             new_dest_path = Path(new_dest_path).with_suffix(".html")
-            generate_page(new_from_path, template_path, new_dest_path)
+            generate_page(new_from_path, template_path, new_dest_path, basepath)
         else:
-            generate_page_recursively(new_from_path, template_path, new_dest_path)
+            generate_page_recursively(new_from_path, template_path, new_dest_path, basepath)
 
-def generate_page(from_path, template_path, dest_path):
+def generate_page(from_path, template_path, dest_path, basepath):
     print(f"* {from_path} --> {dest_path} <-- {template_path}.")
 
     with open(from_path, "r") as f:
@@ -37,7 +37,10 @@ def generate_page(from_path, template_path, dest_path):
     title = extract_title(markdown)
 
     titled_page = re.sub(r"{{ Title }}", title, template)
-    full_page = re.sub(r"{{ Content }}", html_code, titled_page)
+    content_page = re.sub(r"{{ Content }}", html_code, titled_page)
+
+    html_content_page = content_page.replace('href="/', f'href="{basepath}')
+    full_page = html_content_page.replace('src="/', f'src="{basepath}')
 
     dest_dir_path = os.path.dirname(dest_path)
     if not os.path.exists(dest_dir_path):
--- src/main.py
+++ src/main.py
@@ -1,24 +1,27 @@
 from copy_static import copy_to_folder
 from generate import generate_page_recursively
-import os
-# import shutil
+import sys
 
 static_dir = "./static"
-public_dir = "./public"
+public_dir = "./docs"
 
 def main():
 
     try:
-        print("Deleting public directory...")
-        print("Copying static files to public directory...")
+        basepath = "/"
+        if len(sys.argv) > 1:
+            basepath = sys.argv[1]
+
+        print("Deleting docs directory...")
+        print("Copying static files to docs directory...")
         copy_to_folder(static_dir, public_dir)
 
         from_path = "./content"
         template_path = "./template.html"
-        dest_path = "./public"
+        dest_path = "./docs"
 
         print("Generating content...")
-        generate_page_recursively(from_path, template_path, dest_path)
+        generate_page_recursively(from_path, template_path, dest_path, basepath)
 
     except Exception as e:
         print(e)
